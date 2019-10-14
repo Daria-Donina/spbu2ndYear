f0d5db35071dbcf8e6514400eaf0a8393db29b3a
Daria-Donina <donina.daria@yandex.ru>
2019-10-14 18:09:26 +0300

    tests for multithreaded version

---
+++ Semester3/Homework1/Lazy/LazyTests/LazyMultithreadedTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Lazy.Tests
+{
+    [TestClass]
+    public class LazyMultithreadedTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullSupplierTest() => _ = LazyFactory<int>.CreateLazyMultitreaded(null);
+
+        [TestMethod]
+        public void GetTest()
+        {
+            var testString = "test";
+            var lazy = LazyFactory<string>.CreateLazyMultitreaded(() => testString);
+
+            Assert.AreEqual(testString, lazy.Get());
+        }
+
+        [TestMethod]
+        public void RepeatedGetCallTest()
+        {
+            var lazy = LazyFactory<int[]>.CreateLazyMultitreaded(() => (new int[] { 1, 2, 3, 4, 5, 6 }));
+
+            const int amountOfThreads = 10;
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < amountOfThreads; ++i)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    var firstResult = lazy.Get();
+                    Assert.AreEqual(firstResult, lazy.Get());
+                }));
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        [TestMethod]
+        public void ValueIsNotCalculatedMoreThanOnceTest()
+        {
+            int count = 0;
+            var lazy = LazyFactory<int>.CreateLazyMultitreaded(() => ++count);
+
+            const int amountOfThreads = 10;
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < amountOfThreads; ++i)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    const int amountOfCalls = 10;
+                    for (int j = 0; j < amountOfCalls; ++j)
+                    {
+                        Assert.AreEqual(1, lazy.Get());
+                    }
+                }));
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
