d9322690a0f7d4eb8ea1400db984f6796acd7fe5
Daria-Donina <donina.daria@yandex.ru>
2019-11-01 15:25:43 +0300

    added task

---
+++ Semester3/NetworkChat/NetworkChat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                var server = new Server(int.Parse(args[0]));
+                server.Start();
+
+            }
+            else
+            {
+                var client = new Client(int.Parse(args[0]), IPAddress.Parse(args[1]));
+                client.Start();
+            }
+        }
+    }
+}
---
+++ Semester3/NetworkChat/NetworkChat/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkChat
+{
+    class Server
+    {
+        TcpListener listener;
+        public Server(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine($"Listening on port {port}");
+        }
+
+        public async void Start()
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            while (true)
+            {
+                Writer(client.GetStream());
+                Reader(client.GetStream());
+            }
+        }
+
+        public void Writer(NetworkStream stream)
+        {
+            Task.Run(async () =>
+            {
+                var writer = new StreamWriter(stream) { AutoFlush = true };
+                while (true)
+                {
+                    string lineToSend = Console.ReadLine();
+                    await writer.WriteLineAsync(lineToSend);
+                }
+            });
+        }
+
+        public void Reader(NetworkStream stream)
+        {
+            Task.Run(async () =>
+            {
+                var reader = new StreamReader(stream);
+                var data = await reader.ReadLineAsync();
+                Console.WriteLine($"Received: {data}");
+            });
+        }
+    }
+}
