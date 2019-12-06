be5ec942b3885df286dc83703e6c2f5c15b987fe
Daria-Donina <donina.daria@yandex.ru>
2019-12-06 15:20:12 +0300

    added task

---
+++ Semester3/TicTacToe/TicTacToe/Game.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        public bool[][] Map { get; set; }
+
+        public Game()
+        {
+            Map = new bool[3, 3] { };
+        }
+
+        private void Move()
+        {
+
+        }
+
+        private bool IsGameOver()
+        {
+            //Проверка строк
+            for (int i = 0; i < 3; ++i)
+            {
+                if (Map[i][0] == Map[i][1] && Map[i][1] == Map[i][2])
+                {
+                    return true;
+                }
+            }
+
+            //Проверка столбцов
+            for (int j = 0; j < 3; ++j)
+            {
+                if (Map[0][j] == Map[1][j] && Map[1][j] == Map[2][j])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Semester3/TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Game game;
+        public MainWindow()
+        {
+            InitializeComponent();
+            game = new Game();
+        }
+
+        public void OnClickButton(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+
+            var row = button.Name[6];
+            var column = button.Name[7];
+
+            if (true)
+            {
+                button.Content = "x";
+
+                game.Map[row][column] = true;
+            }
+            else
+            {
+                button.Content = "0";
+
+                game.Map[row][column] = false;
+            }
+            button.IsEnabled = false;
+        }
+
+    }
+}
