6fbd5c56cb89b68b6cda58e954a3dc1637e639e2
Daria-Donina <donina.daria@yandex.ru>
2019-12-10 01:57:37 +0300

    added some details to MyTask

--- Semester3/Homework2/MyThreadPool/MyThreadPool/MyThreadPool.cs
+++ Semester3/Homework2/MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -7,6 +8,8 @@ namespace MyThreadPool
     class MyThreadPool
     {
         private List<Thread> threads;
+        private ConcurrentQueue<Action> actions;
+
         public MyThreadPool(int threadNumber)
         {
             if (threadNumber <= 0)
@@ -15,25 +18,63 @@ namespace MyThreadPool
             }
 
             threads = new List<Thread>();
+            actions = new ConcurrentQueue<Action>();
+
             for (int i = 0; i < threadNumber; ++i)
             {
-                threads.Add(new Thread());
+                threads.Add(new Thread(PerformTasks));
+                threads[i].Start();
             }
         }
 
         private class MyTask<TResult> : IMyTask<TResult>
         {
             public bool IsCompleted { get; }
-            public bool Result { get; }
+            public bool Result { get; private set; }
+            private Func<TResult> supplier;
+            ConcurrentQueue<Action> myTaskActions = new ConcurrentQueue<Action>();
+            private TResult result;
+            private object locker = new object();
+
+            public MyTask(Func<TResult> supplier)
+            {
+                this.supplier = supplier;
+            }
+
+            public Action GetAction()
+            {
+                
+            }
+
             public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> supplier)
             {
 
             }
+
+            private void Calculate()
+            {
+                lock (locker)
+                {
+                    result = supplier();
+                }
+            }
         }
 
-        public IMyTask<TResult> TaskQueue<TResult>(Func<TResult> supplier)
+        private void PerformTasks()
         {
-            
+            while (true)
+            {
+                if (actions.TryDequeue(out Action action))
+                {
+                    action.Invoke();
+                }
+            }
+        }
+
+        private void AddTask<TResult>(Func<TResult> supplier)
+        {
+            var task = new MyTask<TResult>(supplier);
+            actions.Enqueue(task);
         }
 
         public void Shutdown()
