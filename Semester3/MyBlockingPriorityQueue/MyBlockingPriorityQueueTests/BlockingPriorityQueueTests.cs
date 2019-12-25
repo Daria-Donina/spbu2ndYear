587b0105304a7b253daa14552c1a369f439bac41
Daria-Donina <donina.daria@yandex.ru>
2019-12-25 13:27:27 +0300

    added task without tests

---
+++ Semester3/MyBlockingPriorityQueue/MyBlockingPriorityQueueTests/BlockingPriorityQueueTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyBlockingPriorityQueue;
+
+namespace MyBlockingPriorityQueueTests
+{
+    [TestClass]
+    public class BlockingPriorityQueueTests
+    {
+        private BlockingPriorityQueue<int> intQueue;
+        [TestInitialize]
+        public void Initialize()
+        {
+            intQueue = new BlockingPriorityQueue<int>();
+        }
+
+        [TestMethod]
+        public void DequeueTest()
+        {
+            intQueue.Enqueue(1, 3);
+            intQueue.Enqueue(2, 7);
+            intQueue.Enqueue(3, 6);
+            intQueue.Enqueue(4, 4);
+        }
+
+        [TestMethod]
+        public void DequeueFromHead()
+        {
+            intQueue.Enqueue(1, 6);
+
+            const int amountOfThreads = 10;
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < amountOfThreads; ++i)
+            {
+                threads.Add(new Thread(() =>
+                {
+                    Assert.AreEqual(1, intQueue.Dequeue());
+                }));
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        [TestMethod]
+        public void DequeueEmpty()
+        {
+
+        }
+    }
+}
---
+++ Semester3/MyBlockingPriorityQueue/MyBlockingPriorityQueueTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyBlockingPriorityQueueTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyBlockingPriorityQueueTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7c0c1fd7-e611-4d46-bab2-5f002cf46d48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
