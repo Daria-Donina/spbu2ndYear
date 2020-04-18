ff8c61c9b91d05c0166731de31357f8418ee5ed0
Daria-Donina <donina.daria@yandex.ru>
2020-04-18 17:43:59 +0300

    trying to fix a problem

--- Semester3/MyNUnit/MyNUnit/NUnit.cs
+++ Semester3/MyNUnit/MyNUnit/NUnit.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Collections.Concurrent;
 using MyNUnit.Attributes;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MyNUnit
 {
@@ -24,67 +27,60 @@ namespace MyNUnit
             {
                 var testClasses = assembly.GetTypes();
 
-                Parallel.ForEach(testClasses, (testClass) =>
+                Parallel.ForEach(testClasses, async (testClass) =>
                 {
-                    var beforeClassMethods = new BlockingCollection<MethodInfo>();
-                    var afterClassMethods = new BlockingCollection<MethodInfo>();
-                    var beforeMethods = new BlockingCollection<MethodInfo>();
-                    var afterMethods = new BlockingCollection<MethodInfo>();
-                    var testMethods = new BlockingCollection<MethodInfo>();
+                    var methods = testClass.GetMethods();
 
-                    #region initialization of collections
-                    Parallel.ForEach(testClass.GetMethods(), (method) =>
-                    {
-                        Parallel.ForEach(Attribute.GetCustomAttributes(method), (attribute) =>
-                        {
-                            if (attribute.GetType() == typeof(BeforeClassAttribute))
-                            {
-                                beforeClassMethods.Add(method);
-                            }
+                    await Task.Run(() => RunBeforeClassMethods(methods.Where(method => MethodSelector<BeforeClassAttribute>(method))));
 
-                            if (attribute.GetType() == typeof(AfterClassAttribute))
-                            {
-                                afterClassMethods.Add(method);
-                            }
+                    await Task.Run(() => ExecuteTests(
+                        methods.Where(method => MethodSelector<BeforeAttribute>(method)),
+                        methods.Where(method => MethodSelector<TestAttribute>(method)),
+                        methods.Where(method => MethodSelector<AfterClassAttribute>(method))));
 
-                            if (attribute.GetType() == typeof(BeforeAttribute))
-                            {
-                                beforeMethods.Add(method);
-                            }
-
-                            if (attribute.GetType() == typeof(AfterAttribute))
-                            {
-                                afterMethods.Add(method);
-                            }
-
-                            if (attribute.GetType() == typeof(TestAttribute))
-                            {
-                                testMethods.Add(method);
-                            }
-                        });
-                    });
-                    #endregion
-
-                    RunBeforeClassMethods(beforeClassMethods);
-
-                    ExecuteTests(beforeMethods, afterMethods, testMethods);
-
-                    RunAfterClassMethods(afterClassMethods);
+                    await Task.Run(() => RunAfterClassMethods(methods.Where(method => MethodSelector<AfterClassAttribute>(method))));
                 });
             });
         }
 
-        private static void RunBeforeClassMethods(BlockingCollection<MethodInfo> methods)
+        private static bool MethodSelector<T>(MethodInfo method) where T : Attribute
+        {
+            var attributes = method.GetCustomAttributes<T>();
+
+            if (attributes.Count() > 1)
+            {
+                //какое?
+                throw new Exception();
+            }
+            else if (attributes.Count() == 0)
+            {
+                return false;
+            }
+
+            if (method.GetParameters().Length != 0 || method.ReturnType != typeof(void))
+            {
+                //какое?
+                throw new Exception();
+            }
+
+            if (typeof(T) == typeof(BeforeClassAttribute) || typeof(T) == typeof(AfterClassAttribute))
+            {
+                if (!method.IsStatic)
+                {
+                    //какое?
+                    throw new Exception();
+                }
+            }
+
+            return true;
+        }
+
+        private static void RunBeforeClassMethods(IEnumerable<MethodInfo> methods)
         {
             Parallel.ForEach(methods, (method) =>
             {
                 try
                 {
-                    if (!method.IsStatic)
-                    {
-                        throw new InvalidOperationException("Method with BeforeClass attribute must be static");
-                    }
-
                     method.Invoke(null, null);
                 }
                 catch (Exception e)
@@ -94,17 +90,12 @@ namespace MyNUnit
             });
         }
 
-        private static void RunAfterClassMethods(BlockingCollection<MethodInfo> methods)
+        private static void RunAfterClassMethods(IEnumerable<MethodInfo> methods)
         {
             Parallel.ForEach(methods, (method) =>
             {
                 try
                 {
-                    if (!method.IsStatic)
-                    {
-                        throw new InvalidOperationException("Method with AfterClass attribute must be static");
-                    }
-
                     method.Invoke(null, null);
                 }
                 catch (Exception e)
@@ -114,60 +105,70 @@ namespace MyNUnit
             });
         }
 
-        private static void ExecuteTests(
-            BlockingCollection<MethodInfo> beforeMethods, 
-            BlockingCollection<MethodInfo> afterMethods,
-            BlockingCollection<MethodInfo> testMethods)
+        private async static void ExecuteTests(
+            IEnumerable<MethodInfo> beforeMethods,
+            IEnumerable<MethodInfo> afterMethods,
+            IEnumerable<MethodInfo> testMethods)
         {
-            Parallel.ForEach(beforeMethods, (method) =>
+            await Task.Run(() =>
             {
-                method.Invoke(method.DeclaringType, null);
+                Parallel.ForEach(beforeMethods, (method) =>
+                {
+                    method.Invoke(method.DeclaringType, null);
+                });
             });
 
-            Parallel.ForEach(testMethods, (test, state) =>
+            await Task.Run(() =>
             {
-                var attribute = test.GetCustomAttribute<TestAttribute>();
-
-                if (!(attribute.Ignore is null))
+                Parallel.ForEach(testMethods, (test, state) =>
                 {
-                    WriteMessage($"Test {test.Name} ignored\n {attribute.Ignore}");
+                    var attribute = test.GetCustomAttribute<TestAttribute>();
 
-                    state.Break();
-                }
-
-                var startTime = Stopwatch.StartNew();
-
-                try
-                {
-                    var result = test.Invoke(test.DeclaringType, null);
-
-                    startTime.Stop();
-
-                    WriteMessage($"Test {test.Name} passed\n Result: {result}");
-                }
-                catch (Exception exception)
-                {
-                    startTime.Stop();
-
-                    if (attribute.Expected == exception.GetType())
+                    if (!(attribute.Ignore is null))
                     {
-                        WriteMessage($"Test {test.Name} passed");
+                        WriteMessage($"Test {test.Name} ignored\n {attribute.Ignore}");
+
+                        state.Break();
                     }
-                    else
+
+                    var startTime = Stopwatch.StartNew();
+
+                    try
                     {
-                        WriteMessage($"Test {test.Name} failed\n Expected {attribute.Expected}," +
-                            $" but received {exception.GetType()}");
+                        var result = test.Invoke(test.DeclaringType, null);
+
+                        startTime.Stop();
+
+                        WriteMessage($"Test {test.Name} passed\n Result: {result}");
                     }
-                }
-                finally
-                {
-                    WriteMessage($"Execution time: {startTime.Elapsed}\n \n");
-                }
+                    catch (Exception exception)
+                    {
+                        startTime.Stop();
+
+                        if (attribute.Expected == exception.GetType())
+                        {
+                            WriteMessage($"Test {test.Name} passed");
+                        }
+                        else
+                        {
+                            WriteMessage($"Test {test.Name} failed\n Expected {attribute.Expected}," +
+                                $" but received {exception.GetType()}");
+                        }
+                    }
+                    finally
+                    {
+                        WriteMessage($"Execution time: {startTime.Elapsed}\n \n");
+                    }
+                });
             });
 
-            Parallel.ForEach(afterMethods, (method) =>
+
+            await Task.Run(() =>
             {
-                method.Invoke(method.DeclaringType, null);
+                Parallel.ForEach(afterMethods, (method) =>
+                {
+                    method.Invoke(method.DeclaringType, null);
+                });
             });
         }
 
